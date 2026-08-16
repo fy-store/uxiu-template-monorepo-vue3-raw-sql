b6f3a75688896890775bd0dac6251e9358d2f2b6
feiyu <feiyucode@gmail.com>
2026-08-16 21:22:24 +0800

    build: 更新构建

--- scripts/moveDist.ts
+++ scripts/moveDist.ts
@@ -1,24 +1,41 @@
 import fs from 'fs-extra'
-import path from 'path/posix'
+import { FileManage } from '../packages/server/src/utils/FileManage'
+import path from 'node:path'
 import { styleText } from 'node:util'
 import { name, description, version } from '../package.json'
 
+const fm = new FileManage()
 try {
 	const root = process.cwd()
 	console.log('')
 	console.log(styleText('green', `正在整合文件...`))
 	console.log('')
 	const target = path.join(root, '/dist')
-	if (fs.pathExistsSync(target)) {
-		fs.rmSync(target, { recursive: true })
+	if (fm.isDirExistSync(target)) {
+		const children = fm.getDirChildrenSync(target)
+		const fileterPathInfo = children.filter((it) => {
+			if (it.isDirectory && it.name === 'node_modules') {
+				return false
+			}
+			if (it.isFile && (it.name === 'package-lock.json' || it.name === 'pnpm-lock.yaml' || it.name === 'yarn.lock')) {
+				return false
+			}
+			return true
+		})
+		await Promise.all(
+			fileterPathInfo.map((it) => {
+				return fm.remove(path.join(it.parentPath, it.name))
+			})
+		)
 	}
-	fs.moveSync(path.join(root, '/packages/server/dist'), target)
-	fs.moveSync(path.join(root, '/packages/web/dist'), path.join(root, '/dist/public'))
-	const pack = JSON.parse(fs.readFileSync(path.join(root, '/dist/package.json'), 'utf-8').toString())
+	fm.createDirSync(root, '/dist')
+	await fm.moveChildren(path.join(root, '/packages/server/dist'), target, { replace: true })
+	await fm.moveChildren(path.join(root, '/packages/web/dist'), path.join(root, '/dist/public'), { replace: true })
+	const pack = JSON.parse(await fm.readToText(path.join(root, '/dist/package.json')))
 	pack.name = name
 	pack.description = description
 	pack.version = version
-	fs.writeFileSync(path.join(root, '/dist/package.json'), JSON.stringify(pack, null, 2))
+	await fm.writeFileFromText(path.join(root, '/dist/package.json'), JSON.stringify(pack, null, 2))
 	console.log(styleText('green', `整合完成: ${path.join(root, 'dist').replaceAll('\\', '/')}`))
 	console.log('')
 	process.exit(0)
