165fa9c053ecf9e5eed7dcee596dd25383081b0d
feiyu <feiyucode@gmail.com>
2026-08-16 20:27:24 +0800

    feat: 提供自定义错误处理函数的默认处理选项

--- packages/server/src/middleware/staticFile/index.ts
+++ packages/server/src/middleware/staticFile/index.ts
@@ -13,8 +13,13 @@ export interface StaticFileOptions {
 	isHandle?: (ctx: Context) => boolean | Promise<boolean>
 	/** 自定义重定向路径, 不进行处理请返回 `filePath` */
 	redirect?: (ctx: Context, filePath: string) => string | Promise<string>
-	/** 自定义错误处理 */
-	errorHandle?: (ctx: Context, error: any) => void | Promise<void>
+	/**
+	 * 自定义错误处理
+	 * @param ctx 上下文
+	 * @param error 错误
+	 * @param defaultErrorHandle 默认错误处理函数, 可调用默认错误处理函数
+	 */
+	errorHandle?: (ctx: Context, error: any, defaultErrorHandle: () => void) => void | Promise<void>
 	/** 自定义 send 选项, 默认值请参考 `@koa/send`, 当前组件默认覆盖了 `setHeaders` 选项以用于更合理的生成响应头 */
 	sendOptions?: Parameters<typeof send>[2]
 }
@@ -42,7 +47,7 @@ export function staticFile(options: StaticFileOptions) {
 		}
 
 		// 不访问公共文件目录，放行
-		if (!ctx.path.replaceAll('\\', '/').startsWith(startPath)) {
+		if (!ctx.path.startsWith(startPath)) {
 			await next()
 			return
 		}
@@ -70,15 +75,18 @@ export function staticFile(options: StaticFileOptions) {
 				}
 			})
 		} catch (error) {
-			if (options.errorHandle) {
-				await options.errorHandle(ctx, error)
-			} else {
+			const defaultErrorHandle = () => {
 				ctx.attachment(name || void 0, { type: disposition })
 				ctx.status = 404
 				ctx.body = {
 					code: 404,
 					msg: '资源未找到'
 				}
+			}
+			if (options.errorHandle) {
+				await options.errorHandle(ctx, error, defaultErrorHandle)
+			} else {
+				defaultErrorHandle()
 			}
 		}
 	}
